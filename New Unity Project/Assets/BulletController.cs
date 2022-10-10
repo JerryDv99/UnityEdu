bbcf5a8c3cfdddd7745ad8f9b71b5c86dc423021
JerryDv99 <devmanjerry@gmail.com>
2022-10-11 00:43:56 +0900

    1

--- New Unity Project/Assets/BulletController.cs
+++ New Unity Project/Assets/BulletController.cs
@@ -8,10 +8,22 @@ public class BulletController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        // 현재 스크립트가 포함된 gameObject를 삭제
         Destroy(this.gameObject, 0.05f);
-        GameObject Obj = Instantiate(Fx);
-        Obj.transform.position = transform.position;
 
-        Obj.transform.LookAt(Camera.main.transform.position);
+        // Instantiate = 복제 함수
+        // Fx의 복사본을 Ob로 넘겨줌
+        GameObject Obj = Instantiate(Fx);
+
+        // 현재 총알 = 이 스크립트가 포함된 gameObject
+        // 총알의 진행방향의 반대방향을 바라보는 벡터를 구함
+        Vector3 Direction = (transform.position - collision.transform.position).normalized;
+
+        // 현재 총알의 위치로부터 Direction 방향으로 2.0 만큼 이동
+        // 결과적으로 총알 진행방향의 반대로 물러남
+        Obj.transform.position = transform.position + (Direction * 2.0f);
+
+        // Fx의 복사본인 Obj를 애니메이션이 끝나는 1.5초 뒤에 삭제
+        Destroy(Obj.gameObject, 1.5f);
     }
 }
--- New Unity Project/Assets/PlayerControler.cs
+++ New Unity Project/Assets/PlayerControler.cs
@@ -10,6 +10,11 @@ public class PlayerControler : MonoBehaviour
     public Transform FirePoint;
     public GameObject BulletPrefab;
     public float Power;
+    public float Radius;
+    public float Vib;
+
+    public LayerMask TargetMask;
+
 
     // 런타임 이후에 처음 한번 실행
     // Start 함수보다 먼저 실행됨
@@ -29,7 +34,6 @@ public class PlayerControler : MonoBehaviour
     void Start()
     {
         Speed = 5.0f;
-
         Power = 0;
         // Rigidbody 컴퍼넌트에 transform.forward 방향으로 500.0f 만큼 힘을 가한다
         //Rigid.AddForce(transform.forward * 2500.0f);
@@ -47,35 +51,108 @@ public class PlayerControler : MonoBehaviour
     void Update()
     {
         // -1 ~ +1 까지 실수 단위로 반환 
-        //float fHor = Input.GetAxis("Horizontal");
-        //float fVer = Input.GetAxis("Vertical");
+        float fHor = Input.GetAxis("Horizontal");
+        float fVer = Input.GetAxis("Vertical");
 
         // -1, 0, 1
-        float fHor = Input.GetAxisRaw("Horizontal");
-        float fVer = Input.GetAxisRaw("Vertical");
+       //float fHor = Input.GetAxisRaw("Horizontal");
+       //float fVer = Input.GetAxisRaw("Vertical");
         
         //Vector3 Movement = Vector3.forward * Speed;
         Vector3 Movement = new Vector3(fHor, 0.0f, fVer) * Speed * Time.deltaTime;
         transform.position += Movement;
 
+        // 버튼이 입력되면 Power 초기화
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Vib = 0;
+            Radius = 0;
             Power = 0;
         }
+        // 버튼을 누르고 있는 동안 Power의 값을 누적시킴
         if (Input.GetKey(KeyCode.Space))
         {
             Power += Time.deltaTime;
-
+            Radius += Time.deltaTime;
+            Vib += Time.deltaTime;
         }
+        // 버튼을 놓았을때
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            // 객체 복제 함수
+            // 타겟의 정보를 받아온다
+            RaycastHit hit;
+
+            // 가상의 선을 발사해서 충돌이 있다면 hit 대상을 미리 확인한다
+            if (Physics.Raycast(FirePoint.position, FirePoint.transform.forward, out hit, 100.0f, TargetMask))
+            {
+                /*
+                Vector3 offset = new Vector3(
+                    Random.Range(-0.5f, +0.5f),
+                    Random.Range(-0.5f, +0.5f), 0.0f);
+                 */
+                Vector3 offset = new Vector3(
+                   Mathf.Cos(90 * Mathf.Deg2Rad),
+                   Mathf.Sin(90 * Mathf.Deg2Rad),
+                    0.1f) * Radius + FirePoint.position ;
+
+                /*
+                float RandomNumber = Random.Range(-1.0f, 1.0f);
+
+                Vector3 Vibrater = new Vector3(
+                   Mathf.Cos(RandomNumber * Mathf.Deg2Rad),
+                   Mathf.Sin(RandomNumber  * Mathf.Deg2Rad),
+                    0.0f) * Vib;
+                */
+                Vector3 Vibrater = new Vector3(
+                    Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0.0f) * Vib;
+
+                Debug.DrawLine(FirePoint.position, offset + Vibrater, Color.red);
+               // Debug.DrawLine(FirePoint.position, FirePoint.position + offset*Radius, Color.red);
+
+                // 총알을 복제
+                GameObject obj = Instantiate(BulletPrefab);
+
+                // hit.point = 현재 hit의 위치
+                // 현재 총알의 위치를 hit.point로 초기화
+                obj.transform.position = offset + Vibrater;
+
+                //Rigidbody Rigid = obj.GetComponent<Rigidbody>();
+
+                // Addforce = 힘을 가한다
+                // 총알이 발사될 시작점에서 Firepoint.transform.forward  방향으로 Power * 1000 의 힘으로 총알을 발사
+                //Rigid.AddForce(FirePoint.transform.forward * 1000);
+                /*
+                if (hit.transform.tag != "Bullet")
+                {
+                    // 총알을 복제
+                    GameObject obj = Instantiate(BulletPrefab);
+
+                    // hit.point = 현재 hit의 위치
+                    // 현재 총알의 위치를 hit.point로 초기화
+                    obj.transform.position = hit.point;
+                }                
+                 */
+            }
+        }
+       
+        /*
+          if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // 객체 복제 함수, 총알을 복제한다
             GameObject obj = Instantiate(BulletPrefab);
+
+            // 복제된 총알의 좌표를 총알이 나가는 위치로 초기화
             obj.transform.position = FirePoint.position;
 
+            // Rigidbody : 물리엔진
+            // obj = 총알
+            // obj에 현재 포함되어있는 Rigidbody Component를 받아온다
             Rigidbody Rigid = obj.GetComponent<Rigidbody>();
+
+            // Addforce = 힘을 가한다
+            // 총알이 발사될 시작점에서 Firepoint.transform.forward  방향으로 Power * 1000 의 힘으로 총알을 발사
             Rigid.AddForce(FirePoint.transform.forward * Power * 1000);
         }
-
+         */
     }
 }
